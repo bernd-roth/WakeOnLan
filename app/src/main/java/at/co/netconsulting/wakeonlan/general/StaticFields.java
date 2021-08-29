4efceddf93b2cde1d8319e6812ad87e9811e4666
Bernd Roth <berndroth0@gmail.com>
2021-08-29 21:04:00 +0200

    * Introducing class StaticFields.java

---
+++ app/src/main/java/at/co/netconsulting/wakeonlan/general/StaticFields.java
@@ -0,0 +1,20 @@
+package at.co.netconsulting.wakeonlan.general;
+
+import android.Manifest;
+
+public class StaticFields {
+    //DbHelper
+    public static final String DATABASE_NAME = "wifi.db";
+    public static final String WIFI_TABLE_NAME = "wifi";
+    public static final String WIFI_COLUMN_ID = "id";
+    //public static final String WIFI_COLUMN_IMAGEVIEW = "imageView";
+    public static final String WIFI_COLUMN_HOSTNAME = "hostname";
+    public static final String WIFI_COLUMN_GROUP_NAME = "groupname";
+    public static final String WIFI_COLUMN_IP_ADDRESS = "ip_address";
+    public static final String WIFI_COLUMN_BROADCAST_ADDRESS = "broadcast";
+    public static final String WIFI_COLUMN_NIC_MAC = "nic_mac";
+    public static final String WIFI_COLUMN_COMMENT = "comment";
+    //DbHelper
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    public static final String ERROR_TAG = "FILE EXCEPTION: ";
+}
